1fce17fdb4afe7bce6e8bdf4b55f9a33ba7c83cb
Jorge Rubio C <jorgerc85@gmail.com>
2014-12-19 17:04:41 -0500

    Added functionality to add questions

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,42 +1,45 @@
-get '/question' do
-  # display a list of all questions
-  # @questions = Question.all
-  # erb :'questions/index'
-end
+# get '/question' do
+#   # display a list of all questions
+#   # @questions = Question.all
+#   # erb :'questions/index'
+# end
 
-get '/question/new' do
-  # return an HTML form for creating a new question
-  # erb :'questions/new'
-end
+# get '/question/new' do
+#   erb :'question/new'
+# end
 
 post '/question' do
-  # create a new question
-  # @question = Question.create(params[:question])
-  # redirect '/question'
+  p params[:survey]
+  @survey = Survey.find(params[:survey])
+  question = @survey.questions.find_or_create_by(params[:question])
+  params[:possibility].each do |k, v|
+    question.possibilities.find_or_create_by(option: v)
+  end
+  erb :'survey/new'
 end
 
-get '/question/:id' do |id|
-  # display a specific question
-  # @question = Question.find id
-  # erb :'questions/single'
-end
+# get '/question/:id' do |id|
+#   # display a specific question
+#   # @question = Question.find id
+#   # erb :'questions/single'
+# end
 
-get '/question/:id/edit' do |id|
-  # return an HTML form for editing a question
-  # @question = Question.find id
-  # erb :'questions/edit'
-end
+# get '/question/:id/edit' do |id|
+#   # return an HTML form for editing a question
+#   # @question = Question.find id
+#   # erb :'questions/edit'
+# end
 
-put '/question/:id' do |id|
-  # update a specific question
-  # @question = Question.find id
-  # @question.update(params[:question])
-  redirect 'entry/#{@question.id}'
-end
+# put '/question/:id' do |id|
+#   # update a specific question
+#   # @question = Question.find id
+#   # @question.update(params[:question])
+#   redirect 'entry/#{@question.id}'
+# end
 
-delete '/question/:id' do |id|
-  # delete a specific question
-  # @question = Question.find id
-  # @question.destroy
-  # redirect '/question'
-end
+# delete '/question/:id' do |id|
+#   # delete a specific question
+#   # @question = Question.find id
+#   # @question.destroy
+#   # redirect '/question'
+# end
