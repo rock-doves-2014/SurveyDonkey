28f7f7f1dc148073721afb02da760eafe4d78495
fete <d1swaay@gmail.com>
2014-12-19 16:05:36 -0500

    added validations for users, suveys, questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  validates :name, presence: true
+  validates :email, uniqueness: true
+
   has_many :user_surveys
   has_many :surveys, through: :user_surveys 
 end
